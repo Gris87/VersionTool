995299f198844dc4a6c8c3a4b7134eb60adf8b60
Gris87 <Gris87@yandex.ru>
2012-08-19 18:31:15 +0400

    Use build_number file

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 
 int main(int argc, char **argv)
 {
-    if (argc!=4)
+    if (argc!=4 && argc!=5)
     {
         printf("Usage:\n");
-        printf("VersionTool.exe MAJOR MINOR FILE\n");
+        printf("VersionTool.exe MAJOR MINOR BUILD_FILE [FILE]\n");
 
         return 1;
     }
@@ -22,19 +22,7 @@ int main(int argc, char **argv)
 
     if (file)
     {
-        char buffer[100];
-
-        while (!feof(file))
-        {
-            fscanf(file, "%s", buffer);
-
-            if (strcmp(buffer, "VERSION_BUILD")==0)
-            {
-                fscanf(file, "%d", &buildNumber);
-                break;
-            }
-        }
-
+        fscanf(file, "%d", &buildNumber);
         fclose(file);
     }
 
@@ -52,19 +40,26 @@ int main(int argc, char **argv)
     // ----------------------------------------------------------------------------
 
     file=fopen(argv[3], "w");
-
-    fprintf(file, "#ifndef VERSION_H\n");
-    fprintf(file, "#define VERSION_H\n");
-    fprintf(file, "\n");
-    fprintf(file, "#define BUILD_DATE \"%s\"\n", buffer);
-    fprintf(file, "#define VERSION_MAJOR %s\n", argv[1]);
-    fprintf(file, "#define VERSION_MINOR %s\n", argv[2]);
-    fprintf(file, "#define VERSION_BUILD %d\n", buildNumber);
-    fprintf(file, "#define VERSION_FULL \"%s.%s.%d\"\n", argv[1], argv[2], buildNumber);
-    fprintf(file, "\n");
-    fprintf(file, "#endif // VERSION_H\n");
-
+    fprintf(file, "%d", buildNumber);
     fclose(file);
+
+    if (argc==5)
+    {
+        file=fopen(argv[4], "w");
+
+        fprintf(file, "#ifndef VERSION_H\n");
+        fprintf(file, "#define VERSION_H\n");
+        fprintf(file, "\n");
+        fprintf(file, "#define BUILD_DATE \"%s\"\n", buffer);
+        fprintf(file, "#define VERSION_MAJOR %s\n", argv[1]);
+        fprintf(file, "#define VERSION_MINOR %s\n", argv[2]);
+        fprintf(file, "#define VERSION_BUILD %d\n", buildNumber);
+        fprintf(file, "#define VERSION_FULL \"%s.%s.%d\"\n", argv[1], argv[2], buildNumber);
+        fprintf(file, "\n");
+        fprintf(file, "#endif // VERSION_H\n");
+
+        fclose(file);
+    }
 
     return 0;
 }
